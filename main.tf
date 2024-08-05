fbb54e2f452595870a02734b7180865fcc4ea327
Anishgoku <anishgoku95@gmail.com>
2024-08-05 12:19:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,4 +8,14 @@ module "network" {
 
 module "web_servers" {
   source             = "./modules/web_servers"
+  vpc_id             = module.network.vpc_id
+  public_subnets     = module.network.public_subnets
+  web_security_group = module.network.web_security_group
+}
+
+module "app_servers" {
+  source                 = "./modules/app_servers"
+  vpc_id                 = module.network.vpc_id
+  private_subnets        = module.network.private_subnets
+  app_security_group     = module.network.app_security_group
 }
